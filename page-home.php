58e814405067078170f08de1f9e480ba601e1af5
Reynald Schrago <simple-itec@schrago.ch>
2019-11-01 19:34:22 +0100

    create home page

---
+++ page-home.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+	<div class="content-area">
+		<main>
+			<section class="slide">
+				<div class="container">
+					<div class="row">Slider</div>
+				</div>
+			</section>
+			<section class="services">
+				<div class="container">
+					<div class="row">
+						Services
+					</div>
+				</div>
+			</section>
+			<section class="middle-area">
+				<div class="container">
+					<div class="row">
+						<div class="news col-md-9">
+                            <p>The content of the home page</p>
+						</div>
+						<aside class="sidebar col-md-3">Sidebar</aside>
+					</div>
+				</div>
+			</section>
+			<section class="map">
+				<div class="container">
+					<div class="row">
+						Map
+					</div>
+				</div>
+			</section>
+		</main>
+	</div>
+<?php get_footer(); ?>
+
