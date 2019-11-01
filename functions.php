a460263daf89635d792905214ae65d47ba723c28
simple-itec <simple-itec@schrago.ch>
2019-11-01 20:58:40 +0100

    Use the function add_them_support to custom the Header of the pages.

--- functions.php
+++ functions.php
@@ -7,3 +7,17 @@ function load_scripts() {
     wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', array(), '4.3.1', 'all');
 }
 add_action('wp_enqueue_scripts', 'load_scripts');
+
+// Main Configuration Function
+function simpleitec_config() {
+
+    // Many features for theme: Posts Formats/Post Thumbnails/Custom Background/Custom Header
+    // Custom Logo/Feed Links/HTML5/Title Tag/Customize Selective Refresh Widgets/Multisite
+    $args = array(
+        'height' => 225,
+        'width' => 1920
+    );
+    add_theme_support('custom-header', $args);
+
+}
+add_action('after_setup_theme','simpleitec_config',0);
