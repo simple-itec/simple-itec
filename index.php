b662db2a3a8ffff6922b6f67636dd1fc6f5eeffb
Reynald Schrago <simple-itec@schrago.ch>
2019-11-02 06:58:35 +0100

    Getting template parts. <get_template_part()> in index.php and create the directory <template-parts> with <content.php> file into it.

--- index.php
+++ index.php
@@ -25,16 +25,7 @@
 							if (have_posts()) {
 								while (have_posts()) {
 									the_post();
-							?>
-									<article>
-										<h2><?php the_title(); ?></h2>
-										<?php the_post_thumbnail('thumbnail'); ?>
-										<p>Posted in <?php echo get_the_date(); ?> by <?php the_author_posts_link(); ?></p>
-										<p>Categories: <?php the_category(' '); ?></p>
-										<p><?php the_tags('Tags: ', ', '); ?></p>
-										<p><?php the_content(); ?></p>
-									</article>
-							<?php
+									get_template_part('template-parts/content');
 								}
 							} else {
 								echo "There's nothing yet to be displayed!";
